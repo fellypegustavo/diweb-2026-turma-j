ceac6c41ec1ec53d1dd71682711150a4eb1773ea
fellypegustavo <epyllefprato@gmail.com>
2026-09-21 09:49:30 -0300

    Add files via upload

---
+++ bibliotecaj.sql
@@ -0,0 +1,37 @@
+/*create database fellype_j;
+
+create table funcionarios
+  (
+   codfunc int not null auto_increment,
+   cpf char(15) not null, 
+   nome varchar(45) not null,
+   salario varchar(45) not null,
+   funcao varchar(45) not null,
+   primary key(codfunc)
+  );
+  
+create table editoras
+  (
+   coded int not null auto_increment,
+   nome varchar(45) not null, 
+   contato varchar(45) not null,
+   primary key(coded)
+  );
+
+create table autores
+ (
+  codaut int not null auto_increment,
+  nome varchar(45) not null,
+  nacionalidade varchar(45) not null,
+  primary key (codaut)
+ );
+ 
+create table usuarios
+ (
+  codus int not null auto_increment,
+  cpf char(15) not null,
+  nome varchar(45) not null,
+  primary key(Codus)
+ );*/
+ 
+ 
